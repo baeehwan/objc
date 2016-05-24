5f92fd4a2d9fae495573b629c19e9b1eaa5f8fce
baeehwan <baeehwan@paran.com>
2016-05-24 11:01:55 +0900

    Initial Commit

---
+++ ObjC/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  ObjC
+//
+//  Created by SDS on 2016. 5. 24..
+//  Copyright (c) 2016년 SDS. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
